874dc1ca037761330d8caa79329e96543bf7035e
Endriel Kuschel <endrielgomes@yahoo.com>
2023-10-28 09:32:18 -0300

    update exercise

--- ex009.py
+++ ex009.py
@@ -2,3 +2,6 @@
 
 mensagem = 'Ontem choveu e hoje está fazendo sol'
 print(mensagem.split()) # Retorna uma lista com as palavras separadas
+
+mensagem2 = 'hoje-vai-fazer-calor-e-a-turma-não-aguenta-mais'
+print(mensagem2.split('-'))
