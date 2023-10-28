416f974f31588ec6f34691d95c841d238006d66f
Endriel Kuschel <endrielgomes@yahoo.com>
2023-10-28 10:34:46 -0300

    new exercise

---
+++ ex010.py
@@ -0,0 +1,15 @@
+# Praticando funções
+
+# Para a criação de funções simples, usa-se a palavra reserva "def"
+
+def hello_world():
+    print('Exemplo')
+    print('Hello World')
+
+hello_world() # Executa função
+
+def eleva_ao_quadrado(num):
+    resultado = num * num
+    print(resultado)
+
+eleva_ao_quadrado(5)
---
+++ ex011.py
@@ -0,0 +1,17 @@
+# Praticando listas
+
+# A lista pode armazenar diferentes tipos de dados sem causar conflitos
+
+lista = [] # Lista vazia (array, vetor)
+
+lista2 = [2, 6, 'Maria', 'João', 2.78]
+
+print(lista2)
+
+lista2.append(4) # Adiciona um valor a lista
+
+print(lista2)
+
+lista2.remove('Maria') # Remove um valor da lista
+
+print(lista2)
